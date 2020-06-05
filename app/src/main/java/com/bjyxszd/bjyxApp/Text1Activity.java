fc2cd149dec532a09a1778476b2bc3d7fa13dc02
y95 <1469858991@qq.com>
2020-06-05 22:36:58 +0800

    练习2

---
+++ app/src/main/java/com/bjyxszd/bjyxApp/Text1Activity.java
@@ -0,0 +1,14 @@
+package com.bjyxszd.bjyxApp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Text1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text1);
+    }
+}
